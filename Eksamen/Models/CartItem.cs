c6aaf38836ef56c0fcc17995062e7f153b5167d1
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2020-06-04 12:49:56 +0200

    added addProductController.s + Cartitem Model, and editet shop.cshtml

---
+++ Eksamen/Models/CartItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eksamen.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; } // SessionID f.eks. 
+
+        public int Quantity { get; set; }
+
+        public System.DateTime DateCreated { get; set; } // Godt at gemme hvornår kunden har lagt noget i kurven
+
+        public System.DateTime Fullfilled { get; set; } // Hvornår er købet gennemført
+
+        public int ProductId { get; set; } //Hjelmens ID 
+
+        public virtual Product Product { get; set; } // Den kan udelades 
+
+
+    }
+}
